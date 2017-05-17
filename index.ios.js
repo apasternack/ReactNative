d3321921159ea908c7c1d836cac29d83da855fd3
Adam Pasternack <adam.pasternack@huntington.com>
2017-05-17 15:55:52 -0400

    refactoring

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,13 @@
 // Import a library to help create component
 import React from 'react';
-import ReactNative from 'react-native;'
+import { Text, AppRegistry } from 'react-native';
 
 
 // Create component
-
+const App = () => (
+    <Text>Some Text</Text>
+  );
 
 // Render it to device
+
+AppRegistry.registerComponent('albums', () => App);
