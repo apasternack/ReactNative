9b8871958a5679b96b8e107f757ffcb730cd44e3
Adam Pasternack <adam.pasternack@gmail.com>
2017-05-23 22:54:18 -0400

    adding margins on images, text size on title

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,12 +5,17 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { 
+        thumbnailStyle,
+        headerContentStyle,
+        headerTextStyle,
+        thumbnailContainerStyle    
+     } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }}
@@ -18,7 +23,7 @@ const AlbumDetail = ({ album }) => {
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
@@ -29,11 +34,20 @@ const AlbumDetail = ({ album }) => {
 const styles = {
     headerContentStyle: {
         flexDirection: 'column',
-        justifyCOntent: 'space-around'
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
     },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 };
 
